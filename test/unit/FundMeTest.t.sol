e787ab95984de4b421ffccd9eefe479207d1d611
Fatima-del-hue <fatimabashir656@gmail.com>
2025-10-20 11:08:26 -0400

    first commit

---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; //100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.getOwner(), msg.sender);
+        //we are checking with the address of this contract,
+        //because the test contract is actually the one whis i calling the contract and is the owner
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); //this means next line should revert
+        fundMe.fund();
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); //the next TX will be send by this user
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithDrawWithASingleFunder() public funded {
+        //Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        //Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithDrawFromMultipleFunders() public funded {
+        //Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank new address
+            // vm.deal new address
+            // hoax does both
+            hoax(address(i), SEND_VALUE);
+
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        uint256 gasStart = gasleft(); //1000
+        vm.txGasPrice(GAS_PRICE);
+        vm.prank(fundMe.getOwner()); //200
+        fundMe.withdraw();
+
+        uint256 gasEnd = gasleft(); //800
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice; //200 * 1
+
+        //Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            fundMe.getOwner().balance
+        );
+    }
+}
+
+// what can we do to work with addresses outside our system?
+// 1. unit
+// tests a specific part of our code
+// 2. integration
+// tests how our code works with other code
+// 3. forked
+// tests our code on a simulated real environment
+// 4. staging
+// tests our code on a real environment that is not production
+
+// we used this command to test the version function
+// forge test --match-test testPriceFeedVersionIsAccurate -vvvvv --fork-url $SEPOLIA_RPC_URL
+
+// forge coverage --fork-url $SEPOLIA_RPC_URL
+// TO CHECK HOW MUCH OF OUR CODE IS COVERED BY TESTS
